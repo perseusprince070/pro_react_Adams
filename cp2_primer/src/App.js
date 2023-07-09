a23b5cbc715685d3e9ab5d85ac5023680bbc5e1e
Perseus Prince <perseus.prince070@gmail.com>
2023-07-09 14:54:06 +0200

    add button

--- cp2_primer/src/App.js
+++ cp2_primer/src/App.js
@@ -19,8 +19,13 @@ export default class App extends Component {
       : "bg-secondary text-white text-center p-2 m-1"
   }
 
+  handleClick = () => this.setState({ count: this.state.count + 1 })
+
   render = () =>
     <h4 className={ this.getClassName(this.state.count) }>
+      <button className="btn btn-info m-2" onClick={ this.handleClick }>
+        Click Me
+      </button>
       Number of things: { this.isEven(this.state.count) }
     </h4>
 }
