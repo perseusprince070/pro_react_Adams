71c5d67f07f75ec625647cb5ea2796832fb5b81f
Perseus Prince <perseus.prince070@gmail.com>
2023-07-09 14:43:31 +0200

    count number selection

--- cp2_primer/src/App.js
+++ cp2_primer/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
-const message = "This is a constant"
+const count = 4
 
 export default class App extends Component {
   render = () =>
     <h4 className="bg-primary text-white text-center p-2 m-1">
-      { message }
+      Number of things: { count }
     </h4>
 }
