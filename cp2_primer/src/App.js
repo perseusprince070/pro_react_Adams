ddd1dd5b6c2675e3cb0c3dd2bd31674a30070046
Perseus Prince <perseus.prince070@gmail.com>
2023-07-09 14:43:54 +0200

    count number selection

--- cp2_primer/src/App.js
+++ cp2_primer/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 
-const count = 4
-
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 5
+    }
+  }
+
+  isEven(val) {
+    return val % 2 === 0 ? "Even" : "Odd"
+  }
+
+  getClassName(val) {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1"
+  }
+
   render = () =>
-    <h4 className="bg-primary text-white text-center p-2 m-1">
-      Number of things: { count }
+    <h4 className={ this.getClassName(this.state.count) }>
+      Number of things: { this.isEven(this.state.count) }
     </h4>
 }
