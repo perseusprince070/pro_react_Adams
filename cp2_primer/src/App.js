a651f52a411d6aec0a2303ddd2df5aa070f17760
Perseus Prince <perseus.prince070@gmail.com>
2023-07-09 11:43:21 +0200

    using expression

--- cp2_primer/src/App.js
+++ cp2_primer/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
+const message = "This is a constant"
+
 export default class App extends Component {
   render = () =>
     <h4 className="bg-primary text-white text-center p-2 m-1">
-      Component Element
+      { message }
     </h4>
 }
