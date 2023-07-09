8e930da7027f6cf0d5bf94ebd157d1d3706b3187
Perseus Prince <perseus.prince070@gmail.com>
2023-07-09 16:40:50 +0200

    add table

--- cp2_primer/src/App.js
+++ cp2_primer/src/App.js
@@ -22,21 +22,24 @@ export default class App extends Component {
   handleClick = () => this.setState({ count: this.state.count + 1 })
 
   render = () =>
-    <div className="container-fluid p-4">
-      <div className="row bg-info text-white p-2">
-        <div className="col-6 font-weight-bold">Value</div>
-        <div className="col-6 font-weight-bold">Even ?</div>
-      </div>
-      <div calssName="row bg-light p-2 border">
-        <div className="col">{ this.state.count }</div>
-        <div className="col-6">{ this.isEven(this.state.count) }</div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <button className="btn btn-info m-2" onClick={ this.handleClick }>
-            Click Me
-          </button>
-        </div>
-      </div>
-    </div>
+    <table className="table table-striped table-bordered table-sm">
+      <thead className="bg-info text-white">
+        <tr><th>Value</th><th>Even ?</th></tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{ this.state.count }</td>
+          <td>{ this.isEven(this.state.count) }</td>
+        </tr>
+      </tbody>
+      <tfoot className="text-center">
+        <tr>
+          <td colSpan="2">
+            <button className="btn btn-info m-2" onClick={ this.handleClick }>
+              Click Me
+            </button>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
 }
