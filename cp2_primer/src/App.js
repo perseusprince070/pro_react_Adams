202163decca0fef3663356432cb0bf486e4090a3
Perseus Prince <perseus.prince070@gmail.com>
2023-07-09 15:55:33 +0200

    add container

--- cp2_primer/src/App.js
+++ cp2_primer/src/App.js
@@ -22,10 +22,21 @@ export default class App extends Component {
   handleClick = () => this.setState({ count: this.state.count + 1 })
 
   render = () =>
-    <h4 className={ this.getClassName(this.state.count) }>
-      <button className="btn btn-info m-2" onClick={ this.handleClick }>
-        Click Me
-      </button>
-      Number of things: { this.isEven(this.state.count) }
-    </h4>
+    <div className="container-fluid p-4">
+      <div className="row bg-info text-white p-2">
+        <div className="col-6 font-weight-bold">Value</div>
+        <div className="col-6 font-weight-bold">Even ?</div>
+      </div>
+      <div calssName="row bg-light p-2 border">
+        <div className="col">{ this.state.count }</div>
+        <div className="col-6">{ this.isEven(this.state.count) }</div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-info m-2" onClick={ this.handleClick }>
+            Click Me
+          </button>
+        </div>
+      </div>
+    </div>
 }
