67032030c9244457c0e5b7dfa2e889c058e40e6b
Perseus Prince <perseus.prince070@gmail.com>
2023-07-10 10:40:31 +0200

    using variable closure

--- cp2_primer/src/example.js
+++ cp2_primer/src/example.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+function myFunc(name) {
+    let myLocalVar = "sunny"
+    let innerFunction = function () {
+        return ("Hello " + name + ". Today is " + myLocalVar + ".")
+    }
+    return innerFunction()
+}
+
+console.log(myFunc("Adam"))
