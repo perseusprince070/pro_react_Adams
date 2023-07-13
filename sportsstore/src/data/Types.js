531bfe4d4b9bcb1cb3c34a8f600032053a97742d
Perseus Prince <perseus.prince070@gmail.com>
2023-07-13 17:23:36 +0200

    add shopconnector

---
+++ sportsstore/src/data/Types.js
@@ -0,0 +1,8 @@
+export const DataType = {
+    PRODUCTS: "products",
+    CATEGORIES: "categories"
+}
+
+export const ActionTypes = {
+    DATA_LOAD: "data_load"
+}
---
+++ sportsstore/src/shop/ShopConnector.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"
+import { Switch, Route, Redirect } from "react-router-dom"
+import { connect } from "react-redux"
+import { loadData } from "../data/ActionCreators"
+import { DataType } from "../data/Types"
+import { Shop } from "./Shop"
+
+const mapStateToProps = (dataStore) => ({
+    ...dataStore
+})
+
+const mapDispatchToProps = {
+    loadData
+}
+
+const filterProducts = (products = [], category) =>
+    (!category || category === "All") ? products : products.filter(p => p.category.toLowerCase() === category.toLoweCase())
+
+export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
+    class extends Component {
+        render() {
+            return <Switch>
+                <Route path="/shop/products/:category?" render={ (routeProps) =>
+                    <Shop {...this.props } { ...routeProps } products={ filterProducts(this.props.products, routeProps.match.params.category) } />
+                } />
+                <Redirect to="/shop/products" />
+            </Switch>
+        }
+
+        componentDidMount() {
+            this.props.loadData(DataType.CATEGORIES)
+            this.props.loadData(DataType.PRODUCTS)
+        }
+    }
+)
