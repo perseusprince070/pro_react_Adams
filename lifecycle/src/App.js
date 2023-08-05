d47112edb55e9f26270a6b74a343169dd2338021
Perseus Prince <perseus.prince070@gmail.com>
2023-08-05 03:36:45 +0200

    change backgroundcolor

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-import { Message } from "./Message";
+// import { Message } from "./Message";
 // import { List } from "./List";
 // import { ExternalCounter } from "./ExternalCounter";
 // import { HooksMessage } from "./HooksMessage";
+import { DirectionDisplay } from "./DirectionDisplay";
 
 export default class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            counterLeft: 0,
-            counterRight: 0
+            counter: 100
         }
     }
 
-    incrementCounter = (counter) => {
-        if (counter === "left") {
-            this.setState({
-                counterLeft: this.state.counterLeft + 1
-            })
-        } else {
-            this.setState({
-                counterRight: this.state.counterRight + 1
-            })
-        }
-    }
+    // incrementCounter = (counter) => {
+    //     if (counter === "left") {
+    //         this.setState({
+    //             counterLeft: this.state.counterLeft + 1
+    //         })
+    //     } else {
+    //         this.setState({
+    //             counterRight: this.state.counterRight + 1
+    //         })
+    //     }
+    // }
 
     // handleChange = () => {
     //     this.setState({
@@ -31,25 +31,30 @@ export default class App extends Component {
     //     })
     // }
 
+    changeCounter = (val) => {
+        this.setState({
+            counter: this.state.counter + val
+        })
+    }
+
     render() {
         console.log("Render App Component")
         return (
             <div className="container text-center">
-                <div className="row p-2">
-                    <div className="col-6">
-                        <Message
-                            message={ `Left: ${ this.state.counterLeft }` }
-                            callback={ () => this.incrementCounter("left") }
-                            text="Increment Left Counter"
-                        />
-                    </div>
-                    <div className="col-6">
-                        <Message
-                            message={ `Right: ${ this.state.counterRight }` }
-                            callback={ () => this.incrementCounter("right") }
-                            text="Increment Right Counter"
-                        />
-                    </div>
+                <DirectionDisplay value={ this.state.counter } />
+                <div className="text-center">
+                    <button
+                        className="btn btn-primary m-1"
+                        onClick={ () => this.changeCounter(-1) }
+                    >
+                        Decrease
+                    </button>
+                    <button
+                        className="btn btn-primary m-1"
+                        onClick={ () => this.changeCounter(1) }
+                    >
+                        Increase
+                    </button>
                 </div>
             </div>
         )
